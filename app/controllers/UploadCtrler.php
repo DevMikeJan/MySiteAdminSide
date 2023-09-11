d16d56fb47e8c29d78c215d09e0f409ea4769245
DevMikeJan <teyabmichaeljan@gmail.com>
2023-09-11 21:31:12 +0800

    UploadedAssets

--- app/controllers/UploadCtrler.php
+++ app/controllers/UploadCtrler.php
@@ -54,7 +54,7 @@ class UploadCtrler extends Controller {
                 'random_number_rar' => mt_rand(),
                 'random_number_img' => mt_rand(),
                 'uploader_id' => $_SESSION['user_id'],
-                'uploader_name' => $_SESSION['user_fname'] & $_SESSION['user_mname'] &  $_SESSION['user_lname'],
+                'uploader_name' => $_SESSION['user_fname'] .' '. $_SESSION['user_mname'] .' '.  $_SESSION['user_lname'],
                 'uploaded_date' => date("Y-m-d H:i:s a"),
                 'error_message' => ''
             ];
